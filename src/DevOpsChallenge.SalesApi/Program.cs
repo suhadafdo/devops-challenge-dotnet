330874f84d8e6d0a6d9f52d5179da50dfc3f5455
suhadafdo <32953192+suhadafdo@users.noreply.github.com>
2022-09-07 19:15:39 +0530

    remove Server Kestrel

--- src/DevOpsChallenge.SalesApi/Program.cs
+++ src/DevOpsChallenge.SalesApi/Program.cs
@@ -7,6 +7,11 @@ using System;
 // Set console title
 Console.Title = "DevOps Challenge Sales API";
 
+public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseKestrel(options => options.AddServerHeader = false)
+            .UseStartup<Startup>();
+
 // Build and run host
 await Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration(config =>
