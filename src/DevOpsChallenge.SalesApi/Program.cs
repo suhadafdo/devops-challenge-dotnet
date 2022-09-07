ae26ef910a5992edeb72f229c01c99a1fcad19e7
suhadafdo <32953192+suhadafdo@users.noreply.github.com>
2022-09-07 19:29:06 +0530

    public issue

--- src/DevOpsChallenge.SalesApi/Program.cs
+++ src/DevOpsChallenge.SalesApi/Program.cs
@@ -7,7 +7,7 @@ using System;
 // Set console title
 Console.Title = "DevOps Challenge Sales API";
 
-public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
             .UseKestrel(options => options.AddServerHeader = false)
             .UseStartup<Startup>();
